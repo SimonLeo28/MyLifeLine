c11756212d93c0cf3fe1122423fe22d3e82ff2f0
SimonLeo28 <simonleoalexanderpcmct2023@gmail.com>
2024-08-08 12:30:27 +0530

    Added the links of Footer elements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,8 @@ const Footer = () => {
       <div className="container mx-auto text-center">
         <p className="text-gray-700">© 2024 Company. All rights reserved.</p>
         <div className="mt-2">
-        <Link to={"/Privacy-Policy"}><a href="#" className="text-gray-700 hover:text-gray-900 mx-2">Privacy Policy</a></Link>
-        <Link to={"/Terms-And-Conditions"}><a href="#" className="text-gray-700 hover:text-gray-900 mx-2">Terms and Conditions</a></Link>
+        <Link to={"https://drive.google.com/file/d/1cr3WaG4IwUpIAQ9Fwrv5MVLKnABFb2gx/view?usp=drive_link"}><a href="#" className="text-gray-700 hover:text-gray-900 mx-2">Privacy Policy</a></Link>
+        <Link to={"https://drive.google.com/file/d/11s1ovcmVojH7zLTIuOMToGt3a_rmciNo/view?usp=drive_link"}><a href="#" className="text-gray-700 hover:text-gray-900 mx-2">Terms and Conditions</a></Link>
         <Link to={"/Contact"}><a href="#" className="text-gray-700 hover:text-gray-900 mx-2">Contact Us</a></Link>
         </div>
       </div>
